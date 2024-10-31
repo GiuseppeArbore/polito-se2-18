76c5915a341d40a4c9f867afd13691da565f29c0
ALBERTO PEDALINO s247382 <s247382@studenti.polito.it>
2024-10-31 19:50:16 +0100

    test integration

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,22 +4,32 @@ import {validateRequest} from './errorHandlers';
 import { body } from 'express-validator';
 import { AreaType, KxDocumentType, Scale, Stakeholders } from './models/enum';
 
-export const validateKxDocument = () => [
-    body('title').notEmpty().withMessage('Title is required'),
-    body('stakeholders').isArray().withMessage('Stakeholders must be an array')
-        .custom((value) => value.every((v: string) => Object.values(Stakeholders).includes(v as Stakeholders)))
-        .withMessage('Invalid stakeholder value'),
-    body('scale').isNumeric().withMessage('Scale must be a number'),
-    body('scale_info').isIn(Object.values(Scale)).withMessage('Invalid scale value'),
-    body('issuance_date').isISO8601().toDate().withMessage('Issuance date must be a valid date'),
-    body('type').isIn(Object.values(KxDocumentType)).withMessage('Invalid document type'),
-    body('connections').isNumeric().withMessage('Connections must be a number'),
-    body('language').isString().withMessage('Language must be a string'),
-    body('area_type').isIn(Object.values(AreaType)).withMessage('Invalid area type value'),
-    body('description').notEmpty().withMessage('Description is required'),
-];
 
-function initRoutes(app: Application) {
+
+export function initRoutes(app: Application) {
+
+     const validateKxDocument = () => [
+        body('title').notEmpty().withMessage('Title is required'),
+        body('stakeholders').notEmpty().withMessage('Stakeholders are required')
+            .isArray().withMessage('Stakeholders must be an array')
+            .custom((value) => value.every((v: string) => Object.values(Stakeholders).includes(v as Stakeholders)))
+            .withMessage('Invalid stakeholder value'),
+        body('scale').notEmpty().withMessage('Scale is required')
+            .isNumeric().withMessage('Scale must be a number'),
+        body('scale_info').notEmpty().withMessage('Scale info is required')
+            .isIn(Object.values(Scale)).withMessage('Invalid scale value'),
+        body('issuance_date').notEmpty().withMessage('Issuance date is required')
+            .isISO8601().toDate().withMessage('Issuance date must be a valid date'),
+        body('type').notEmpty().withMessage('Type is required')
+            .isIn(Object.values(KxDocumentType)).withMessage('Invalid document type'),
+        body('connections').notEmpty().withMessage('Connections are required')
+            .isNumeric().withMessage('Connections must be a number'),
+        body('language').notEmpty().withMessage('Language is required')
+            .isString().withMessage('Language must be a string'),
+        body('area_type').notEmpty().withMessage('Area type is required')
+            .isIn(Object.values(AreaType)).withMessage('Invalid area type value'),
+        body('description').notEmpty().withMessage('Description is required'),
+    ];
     
     app.get("/doc", async (req, res) => {
         res.status(200).json({ ok: "ok" });
--- server/test_integration/document.test.ts
+++ server/test_integration/document.test.ts
@@ -42,7 +42,7 @@ describe("Integration Tests for Document API", () => {
         documentIds.push(response.body._id);
     });
 
-    test("Test 2 - Should fail to create a document with missing required fields", async () => {
+    test("Test 2 - Should fail to create a document with missing required fields (title)", async () => {
         const response = await request(app)
             .post('/api/documents')
             .send({
@@ -80,6 +80,58 @@ describe("Integration Tests for Document API", () => {
             });
         expect(response.status).toBe(400);
         expect(response.body).toEqual({ error: 'Internal Server Error', status: 400 });
+    });
+
+    test("Test 4 - Should return 500 if there is a database error", async () => {
+        
+        const originalCreateKxDocument = db.createKxDocument;
+        db.createKxDocument = async () => { 
+            const error = new Error('Database error');
+            (error as any).customCode = 500;
+            throw error;
+        };
+
+        const response = await request(app)
+            .post('/api/documents')
+            .send({
+                _id: documentIds[0],
+                title: "Integration Test Document",
+                stakeholders: [Stakeholders.RESIDENT],
+                scale_info: Scale.TEXT,
+                scale: 10,
+                issuance_date: date,
+                type: KxDocumentType.INFORMATIVE,
+                connections: 0,
+                language: "Swedish",
+                area_type: AreaType.ENTIRE_MUNICIPALITY,
+                description: "This is a test document for integration testing."
+            });
+
+        expect(response.status).toBe(500);
+        expect(response.body).toEqual({ error: 'Internal Server Error', status: 500 });
+
+        
+        db.createKxDocument = originalCreateKxDocument;
+    });
+
+
+    test("Test 5 - Should fail to create a document with multiple missing required fields (title,stakeholders)", async () => {
+        const response = await request(app)
+            .post('/api/documents')
+            .send({
+                scale_info: Scale.TEXT,
+                scale: 10,
+                issuance_date: date,
+                type: KxDocumentType.INFORMATIVE,
+                connections: 0,
+                language: "Swedish",
+                area_type: AreaType.ENTIRE_MUNICIPALITY,
+                description: "This is a test document with missing title."
+            });
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toBeDefined();
+        expect(response.body.errors[0].msg).toBe('Title is required');
+        expect(response.body.errors[1].msg).toBe('Stakeholders are required');
     });
 
 });
