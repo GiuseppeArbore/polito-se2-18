36176344e46bba9ac7a2e2eb24d21700032de23c
Stefano Di Leo <>
2024-11-09 22:56:40 +0100

    migrate to Typegoose
    This allows us to define data structures and schema in one place,
    reducing errors

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,8 +3,8 @@ import { createKxDocument, getAllKxDocuments} from './controller';
 import { validateRequest } from './errorHandlers';
 import e, { Application } from 'express';
 import { body } from 'express-validator';
-import { AreaType, isDocCoords, KxDocumentType, Scale, Stakeholders } from './models/enum';
-import { coordDistance, KIRUNA_COORDS } from './utils';
+import { AreaType, KxDocumentType, Stakeholders } from './models/enum';
+import { coordDistance, isDocCoords, KIRUNA_COORDS } from './utils';
 
 
 
@@ -18,23 +18,21 @@ export function initRoutes(app: Application) {
             .withMessage('Invalid stakeholder value'),
         body('scale').notEmpty().withMessage('Scale is required')
             .isNumeric().withMessage('Scale must be a number'),
-        body('scale_info').notEmpty().withMessage('Scale info is required')
-            .isIn(Object.values(Scale)).withMessage('Invalid scale value'),
         body('issuance_date').notEmpty().withMessage('Issuance date is required')
             .isISO8601().toDate().withMessage('Issuance date must be a valid date'),
         body('type').notEmpty().withMessage('Type is required')
             .isIn(Object.values(KxDocumentType)).withMessage('Invalid document type'),
         body('language').optional().notEmpty().withMessage('Language is required')
             .isString().withMessage('Language must be a string'),
-        // body('doc_coordinates').notEmpty().withMessage('Document coordinates are required').isObject()
-        //     .custom((v) => {
-        //         return isDocCoords(v) &&  
-        //             (
-        //                 (v.type === AreaType.ENTIRE_MUNICIPALITY) ||
-        //                 (v.type === AreaType.POINT && coordDistance(v.coordinates as [number, number], KIRUNA_COORDS) < 100) ||
-        //                 (v.type === AreaType.AREA && v.coordinates.every(c => c.every(c => coordDistance(c as [number, number], KIRUNA_COORDS) < 100)))
-        //             )
-        //     }).withMessage('Invalid document coordinates'),
+        body('doc_coordinates').notEmpty().withMessage('Document coordinates are required').isObject()
+            .custom((v) => {
+                return isDocCoords(v) &&  
+                    (
+                        (v.type === AreaType.ENTIRE_MUNICIPALITY) ||
+                        (v.type === AreaType.POINT && coordDistance(v.coordinates as [number, number], KIRUNA_COORDS) < 100) ||
+                        (v.type === AreaType.AREA && v.coordinates.every(c => c.every(c => coordDistance(c as [number, number], KIRUNA_COORDS) < 100)))
+                    )
+            }).withMessage('Invalid document coordinates'),
         body('description').notEmpty().withMessage('Description is required'),
         body('pages').optional().isArray().custom((v) => {
             if (!Array.isArray(v))
