feac07b4f0e420a15eb6878bfd0f9d7e5c5a671a
GiuseppeArbore <s329535@studenti.polito.it>
2024-11-24 15:54:43 +0100

    add sidebar and fully responsivity

--- client/src/components/Console.tsx
+++ client/src/components/Console.tsx
@@ -11,6 +11,7 @@ import {
   Tab,
   Button,
 } from "@tremor/react";
+import "../css/dashboard.css"
 import API from "../API";
 import { useState, useEffect } from "react";
 import { FormDialog } from "./form/Form";
@@ -21,7 +22,7 @@ import { Toaster } from "./toast/Toaster";
 import { toast } from "../utils/toaster";
 import { FeatureCollection } from "geojson";
 import { Link } from "react-router-dom";
-import { RiHome2Fill, RiEyeOffFill, Ri } from "@remixicon/react";
+import { RiHome2Fill, RiEyeOffFill, RiLineFill, RiArrowRightSLine, RiArrowLeftSLine } from "@remixicon/react";
 export default function Console() {
   const [documents, setDocuments] = useState<KxDocument[]>([]);
   const [selectedView, setSelectedView] = useState(0);
@@ -99,6 +100,12 @@ export default function Console() {
   }, [selectedView, refreshNeeded]);
 
   const [showSideBar, setShowSideBar] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <main>
@@ -129,51 +136,67 @@ export default function Console() {
           </TabList>
         </TabGroup>
       </div>
-      {!showSideBar && <Button onClick={() => setShowSideBar(true)}>Show Sidebar</Button>}
-
-
 
       <Grid numItemsLg={6} className="gap-6 mt-6">
         <Col numColSpanLg={showSideBar ? 5 : 6}>
+          <div className="h-full" style={{ display: 'flex', flexDirection: 'row' }}>
+            <Col className="h-full w-full">
+              <Card className="h-full p-0 m-0" style={{ margin: 0, padding: 0, minHeight: "500px" }}>
+                {renderCurrentSelection(selectedView)}
+              </Card>
+            </Col>
+            {!showSideBar &&
+              <Col className="hider ml-2 hide-on-small">
+                <i className="h-full" onClick={() => setShowSideBar(true)}><RiArrowLeftSLine className="h-full"></RiArrowLeftSLine></i>
+              </Col>
+            }
+          </div>
 
-            <Card className="h-full p-0 m-0" style={{ margin: 0, padding: 0, minHeight: "500px" }}>
-            {renderCurrentSelection(selectedView)}
-          </Card>
         </Col>
-        {showSideBar &&
-          <Col numColSpanLg={1}>
-            <Button onClick={() => setShowSideBar(false)}>Hide Sidebar</Button>
+        <Col numColSpanLg={1}>
+          <div className="flex flex-row ">
+            {showSideBar &&
+              <Col className="hider mr-1 hide-on-small">
+                <i className="h-full" onClick={() => setShowSideBar(false)}><RiArrowRightSLine className="h-full"></RiArrowRightSLine></i>
+              </Col>
+            }
+            {(showSideBar || windowWidth <= 1024) &&
+              <Col className="w-full">
+                <div className="space-y-6">
+                  <FormDialog
+                    documents={documents}
+                    refresh={() => setRefreshNeeded(true)}
+                  />
 
-            <div className="space-y-6">
-              <FormDialog
-                documents={documents}
-                refresh={() => setRefreshNeeded(true)}
-              />
+                  <Card>
+                    <Metric>KIRUNA</Metric>
+                    <Title>Quick facts</Title>
+                    <Text>
+                      <ul className="list-disc list-inside">
+                        <li>20,000 inhabitants</li>
+                        <li>Located 140 km north of the Arctic Circle</li>
+                        <li>Lowest recorded temperature -42 °C</li>
+                        <li>45 days of Midnight Sun each year</li>
+                        <li>21 days of Polar Night</li>
+                        <li>Covered in snow for 8 months each year</li>
+                      </ul>
+                    </Text>
+                  </Card>
+                  <Card className="hidden lg:block w-full h-40">
+                    <img
+                      src="/kiruna.png"
+                      alt="Kiruna"
+                      className="w-full h-full object-contain"
+                    />
+                  </Card>
+                </div>
+              </Col>
+            }
 
-              <Card>
-                <Metric>KIRUNA</Metric>
-                <Title>Quick facts</Title>
-                <Text>
-                  <ul className="list-disc list-inside">
-                    <li>20,000 inhabitants</li>
-                    <li>Located 140 km north of the Arctic Circle</li>
-                    <li>Lowest recorded temperature -42 °C</li>
-                    <li>45 days of Midnight Sun each year</li>
-                    <li>21 days of Polar Night</li>
-                    <li>Covered in snow for 8 months each year</li>
-                  </ul>
-                </Text>
-              </Card>
-              <Card className="hidden lg:block w-full h-40">
-                <img
-                  src="/kiruna.png"
-                  alt="Kiruna"
-                  className="w-full h-full object-contain"
-                />
-              </Card>
-            </div>
-          </Col>
-        }
+          </div>
+
+
+        </Col>
 
       </Grid>
 
