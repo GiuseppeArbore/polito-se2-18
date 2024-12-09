9e48c37f6e99322888cf170b554d9d27af8b5b10
ALBERTO PEDALINO s247382 <s247382@studenti.polito.it>
2024-12-09 15:48:59 +0100

    home refactor

--- client/src/components/landing/Home.tsx
+++ client/src/components/landing/Home.tsx
@@ -6,22 +6,20 @@ import { Stakeholders } from "../../enum";
 import { Toaster } from "../toast/Toaster";
 
 interface HomeProps {
-    login: (credentials: { username: string; password: string }) => void;
-    logout: () => void;
-    setError: React.Dispatch<React.SetStateAction<boolean | undefined>>;
-    loginErrorMessage: { msg: string; type: string };
-    error: boolean | undefined;
+
     user: { email: string; role: Stakeholders } | null;
 }
-const Home: React.FC<HomeProps> = ({ login, logout, setError, loginErrorMessage, error, user }) => {
+const Home: React.FC<HomeProps> = ({ user }) => {
     return (
-        <div className="App">
-            <HeroSection login={login} logout={logout} setError={setError} loginErrorMessage={loginErrorMessage} error={error} user={user} />
+        <div className="App" style={{ background: 'url(../../../public/home/KirunaWhiteMap.png) no-repeat center center fixed', backgroundSize: 'cover', height: '93vh', width: '98vw', display: 'flex', flexDirection: 'column', margin: '0', borderRadius: '0.5rem' }}>
+            <HeroSection user={user} />
             <Phases />
-            <FeaturesSection />
             <Footer />
         </div>
     );
 };
+
+
+
 
 export default Home;
--- client/src/components/landing/Phases/Phases.tsx
+++ client/src/components/landing/Phases/Phases.tsx
@@ -3,7 +3,7 @@ import ProcessCarousel from "./ProcessCarousel";
 
 const Phases: React.FC = () => {
     return (
-        <section className="phases-section">
+        <section className="phases-section h-[55vh]">
             <h2 className="text-white text-sm md:text-xl lg:text-2xl xl:text-3xl font-bold ">
                 Phases of The Project
             </h2>
