8c085847ff450c7ba62af0c71f10f7a38e0a7bf9
ALBERTO PEDALINO s247382 <s247382@studenti.polito.it>
2024-12-09 18:57:26 +0100

    adjust phases positions

--- client/src/components/landing/Phases/Phases.tsx
+++ client/src/components/landing/Phases/Phases.tsx
@@ -3,11 +3,13 @@ import ProcessCarousel from "./ProcessCarousel";
 
 const Phases: React.FC = () => {
     return (
-        <section className="phases-section h-[55vh]">
-            <h2 className="text-white text-sm md:text-xl lg:text-2xl xl:text-3xl font-bold ">
-                Phases of The Project
-            </h2>
-            <ProcessCarousel />
+        <section className="phases-section h-[55vh] flex justify-center items-center">
+            <div className="flex flex-col items-center">
+                <h2 className="text-white text-sm md:text-xl lg:text-2xl xl:text-3xl font-bold">
+                    Phases of The Project
+                </h2>
+                <ProcessCarousel />
+            </div>
         </section>
     );
 };
