8fc605b4d6fbb73d8c7dc4244b7a6d780ebc16a8
ALBERTO PEDALINO s247382 <s247382@studenti.polito.it>
2024-12-09 12:53:37 +0100

    Refactor dashboard ui

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,32 @@
+// Layout.tsx
+import React from 'react';
+import Navbar from './Navbar'; // Adjust the import based on your file structure
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+  login: (credentials: { username: string; password: string }) => Promise<void>;
+  logout: () => void;
+  loginErrorMessage: { msg: string; type: string };
+  error: boolean | undefined;
+  setError: (error: boolean) => void;
+  user: any;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, login, logout, loginErrorMessage, error, setError, user }) => {
+  return (
+    <div>
+      <Navbar
+        login={login}
+        logout={logout}
+        loginErrorMessage={loginErrorMessage}
+        error={error}
+        setError={setError}
+        user={user}
+      />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
