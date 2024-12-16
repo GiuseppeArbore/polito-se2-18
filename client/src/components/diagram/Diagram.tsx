ea93071d0f8a5640baf73d7c82d68efbe984d90c
Luca Bordino <lb@Lucas-MacBook-Pro.local>
2024-12-16 12:10:44 +0100

    add year

--- client/src/components/diagram/Diagram.tsx
+++ client/src/components/diagram/Diagram.tsx
@@ -16,13 +16,32 @@ import {
 import dagre from "@dagrejs/dagre";
 import "@xyflow/react/dist/style.css";
 import { KxDocument } from "../../model";
-import { Button } from '@tremor/react';
+import { Button } from "@tremor/react";
 import { on } from "events";
 
 interface FlowProps {
   documents: KxDocument[];
 }
 function Flow(props: FlowProps) {
+  const mapYearType: Map<number, Map<string, number>> = props.documents
+    .map((d) => ({
+      year: new Date(d.issuance_date.from).getFullYear(),
+      type: d.type,
+    }))
+    .reduce<Map<number, Map<string, number>>>((acc, val) => {
+      const yearMap = acc.get(val.year) || new Map<string, number>();
+      const count = yearMap.get(val.type) || 0;
+      yearMap.set(val.type, count + 1);
+      acc.set(val.year, yearMap);
+      return acc;
+    }, new Map<number, Map<string, number>>());
+
+  const mapYearMaxDocuments: Map<number, number> = new Map(
+    mapYearType.entries().map(([year, typeMap]) => {
+      return [year, Math.max(...typeMap.values())];
+    })
+  );
+
   const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
 
   const nodeWidth = 172;
@@ -31,7 +50,11 @@ function Flow(props: FlowProps) {
   const [initialEdges] = useState<Edge[]>([]);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const getLayoutedElements = (nodes:Node[], edges:Edge[], direction = "LR") => {
+  const getLayoutedElements = (
+    nodes: Node[],
+    edges: Edge[],
+    direction = "LR"
+  ) => {
     const isHorizontal = direction === "LR";
     dagreGraph.setGraph({ rankdir: direction });
 
@@ -45,12 +68,12 @@ function Flow(props: FlowProps) {
 
     dagre.layout(dagreGraph);
 
-    const newNodes = nodes.map((node:Node) => {
+    const newNodes = nodes.map((node: Node) => {
       const nodeWithPosition = dagreGraph.node(node.id);
       const newNode = {
         ...node,
-        targetPosition: isHorizontal ? 'left' : 'top',
-        sourcePosition: isHorizontal ? 'right' : 'bottom',
+        targetPosition: isHorizontal ? "left" : "top",
+        sourcePosition: isHorizontal ? "right" : "bottom",
         // We are shifting the dagre node position (anchor=center center) to the top left
         // so it matches the React Flow node anchor point (top left).
         position: {
@@ -58,11 +81,11 @@ function Flow(props: FlowProps) {
           y: nodeWithPosition.y - nodeHeight / 2,
         },
       };
-   
+
       return newNode as Node;
     });
 
-    return { nodes: newNodes, edges } as { nodes:Node[], edges:Edge[] };
+    return { nodes: newNodes, edges } as { nodes: Node[]; edges: Edge[] };
   };
 
   useMemo(() => {
@@ -80,7 +103,7 @@ function Flow(props: FlowProps) {
           },
         };
       });
-    
+
     let connections = props.documents.map((d, index_e) => {
       let a = Object.values(d.connections).map((c, index) => {
         return c.map((i: { toString: () => any }, index_i: number) => {
@@ -97,32 +120,31 @@ function Flow(props: FlowProps) {
     });
     const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
       node_list,
-      connections.flat(),
+      connections.flat()
     );
     setNodes(layoutedNodes);
     setEdges(layoutedEdges);
   }, [props.documents]);
- 
-  
+
   const onConnect = useCallback(
     (params: any) =>
       setEdges((eds) =>
         addEdge(
           { ...params, type: ConnectionLineType.SmoothStep, animated: true },
-          eds,
-        ),
+          eds
+        )
       ),
-    [],
+    []
   );
   const onLayout = useCallback(
     (direction: string | undefined) => {
       const { nodes: layoutedNodes, edges: layoutedEdges } =
         getLayoutedElements(nodes, edges, direction);
- 
+
       setNodes([...layoutedNodes]);
       setEdges([...layoutedEdges]);
     },
-    [nodes, edges],
+    [nodes, edges]
   );
   return (
     <ReactFlow
@@ -130,13 +152,13 @@ function Flow(props: FlowProps) {
       edges={edges}
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
-      onConnect={onConnect}  
+      onConnect={onConnect}
       connectionLineType={ConnectionLineType.SmoothStep}
       fitView
       style={{ backgroundColor: "#F7F9FB" }}
     >
       <Panel position="top-right">
-        <Button onClick={() => onLayout('LR')}>Format Diagram</Button>
+        <Button onClick={() => onLayout("LR")}>Format Diagram</Button>
       </Panel>
       <Controls />
       <Background />
