f93dd120eb59b98a6844f96ac178d06b66b97f5a
Luca Bordino <lb@Lucas-MacBook-Pro.local>
2024-12-16 11:33:27 +0100

    WIP diagram

--- client/src/components/diagram/Diagram.tsx
+++ client/src/components/diagram/Diagram.tsx
@@ -1,80 +1,103 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from "react";
 import {
-    ReactFlow,
-    MiniMap,
-    Controls,
-    Background,
-    useNodesState,
-    useEdgesState,
-    addEdge,
-    Node
-} from '@xyflow/react';
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Node,
+  Edge,
+} from "@xyflow/react";
 
-import '@xyflow/react/dist/style.css';
-import { KxDocument } from '../../model';
-
-const doc_width = 2;
-const min_width = 4;
-
-
-
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
-
+import "@xyflow/react/dist/style.css";
+import { KxDocument } from "../../model";
+import { set } from "date-fns";
 
 interface FlowProps {
-    documents: KxDocument[];
+  documents: KxDocument[];
 }
 function Flow(props: FlowProps) {
-    const mapYearType: Map<number, Map<string, number>> = props.documents.map(d => ({year: new Date(d.issuance_date.from).getFullYear(), type: d.type})).reduce<Map<number, Map<string, number>>>((acc, val) => {
-        
-        const yearMap = acc.get(val.year) || new Map<string, number>();
-        const count = yearMap.get(val.type) || 0;
-        yearMap.set(val.type, count + 1);
-        acc.set(val.year, yearMap);
-        return acc;
-    }, new Map<number, Map<string, number>>());
+  const [initialNodes] = useState<Node[]>([]);
+  const [initialEdges] = useState<Edge[]>([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-
-    const mapYearMaxDocuments: Map<number, number> = new Map(mapYearType.entries().map( ([year, typeMap]) => {
-        return [year, Math.max(...typeMap.values())];
-    }));
-
-
-    let initialNodes: Node[] = props.documents.filter(d => d._id !== undefined).map((d) => {
+  useMemo(() => {
+    let node_list: Node[] = props.documents
+      .filter((d) => d._id !== undefined)
+      .map((d) => {
         return {
-            id: d._id!.toString(),
-            data: {
-                label: d.title
-            },
-            position: {
-                y: Math.random() * 100,
-                x: Math.random() * 100
-            }
+          id: d._id!.toString(),
+          position: {
+            y: Math.floor(Math.random() * 300),
+            x: Math.floor(Math.random() * 300),
+          },
+          data: {
+            label: d.title,
+          },
         };
+      });
+    setNodes(node_list);
+    let connections = props.documents.map((d) => {
+      let edges = d.connections.collateral.map((c, index) => {
+        return {
+          id: (index + 1000).toString(),
+          source: d._id!.toString(),
+          target: c.toString(),
+        };
+      });
+      edges = edges.concat(
+        d.connections.direct.map((c, index) => {
+          return {
+            id: (index + 2000).toString(),
+            source: d._id!.toString(),
+            target: c.toString(),
+          };
+        })
+      );
+      edges = edges.concat(
+        d.connections.projection.map((c, index) => {
+          return {
+            id: (index + 3000).toString(),
+            source: d._id!.toString(),
+            target: c.toString(),
+          };
+        })
+      );
+      edges = edges.concat(
+        d.connections.update.map((c, index) => {
+          return {
+            id: (index + 4000).toString(),
+            source: d._id!.toString(),
+            target: c.toString(),
+          };
+        })
+      );
+      return edges;
     });
-    initialNodes = [{
-        id: '1',
-        data: { label: 'Node 1' },
-        position: { x: 250, y: 50 }
-    }];
-    console.log(initialNodes);
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    console.log(nodes);
-    const onConnect = useCallback((params: any) => setEdges((eds: any) => addEdge(params, eds)), [setEdges]);
+    setEdges(connections.flat());
 
-    return (
-        <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-        >
-            <Controls />
-            <Background />
-        </ReactFlow>
-    );
+  }, [props.documents]);
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds: any) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+    >
+      <Controls />
+      <Background />
+    </ReactFlow>
+  );
 }
 
 export default Flow;
